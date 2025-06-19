8e671d3f6066f544b83bb18bd601923228e3e6be
unknown <ap166884@gmail.com>
2025-06-19 18:03:13 +0700

    аналогично для заданий с репозиторием

--- src/TaskWithRepository/CRUDTaskWithRepository.java
+++ src/TaskWithRepository/CRUDTaskWithRepository.java
@@ -1,13 +1,14 @@
 package TaskWithRepository;
 
 import AlgorithmicTask.AlgorithmicTask;
+import Course.Course;
+import Course.CoursesStorage;
+import CourseModule.CourseModule;
 import Task.TasksStorage;
 
 import java.util.Scanner;
 
 import static ProgramSystem.Utils.readIntInput;
-
-//TODO интеграция в родительский класс
 
 public class CRUDTaskWithRepository {
     public static void createTaskWithRepository() {
@@ -30,6 +31,70 @@ public class CRUDTaskWithRepository {
         System.out.print("Введите пример: ");
         String taskExample = scanner.nextLine();
         taskWithRepository.setTaskExample(taskExample);
+
+        System.out.println("Прикрепить задание к теме?");
+        System.out.println("1. Да");
+        System.out.println("2. Нет");
+        System.out.print("Выберите опцию: ");
+        int choiceForConnect = readIntInput();
+        boolean isSet = false;
+
+        while (!isSet) {
+            if (choiceForConnect != 1 && choiceForConnect != 2) {
+                System.out.print("Ошибка, вы ввели неверный номер! Повторите попытку: ");
+                choiceForConnect = readIntInput();
+            }
+            else {
+                isSet = true;
+            }
+        }
+
+        switch (choiceForConnect) {
+            case 1: {
+                if (CoursesStorage.getCourses().isEmpty()) {
+                    System.out.println("Список классов пуст, сперва создайте класс!");
+                    return;
+                }
+
+                CoursesStorage.writeAllCourses();
+                System.out.print("Выберите класс, к которому будет прикреплено задание: ");
+                int courseNumber = readIntInput();
+
+                if (courseNumber < 1 || courseNumber > CoursesStorage.getCourses().size()) {
+                    System.out.println("Ошибка, вы ввели неверный номер!");
+                    System.out.println("Задание не было прикреплено!");
+                    return;
+                }
+
+                Course course = CoursesStorage.getCourses().get(courseNumber - 1);
+                if (course.getModules().isEmpty()) {
+                    System.out.println("Список модулей пуст, сперва создайте модуль в классе!");
+                    return;
+                }
+
+                course.writeModules(course);
+                System.out.print("Выберите модуль, к которому будет прикреплено задание: ");
+                int moduleNumber = readIntInput();
+
+                if (moduleNumber < 1 || moduleNumber > course.getModules().size()) {
+                    System.out.println("Ошибка, вы ввели неверный номер!");
+                    System.out.println("Задание не было прикреплено!");
+                    return;
+                }
+
+                CourseModule module = course.getModules().get(moduleNumber - 1);
+                module.addTask(taskWithRepository);
+                System.out.println("Задание прикреплено!");
+                break;
+            }
+            case 2: {
+                System.out.println("Задание не прикреплено к теме. Вы можете прикрепить его позднее");
+                break;
+            }
+            default: {
+                System.out.println("Ошибка, неверная команда!");
+            }
+        }
 
         TasksWithRepositoryStorage.addTaskWithRepository(taskWithRepository);
         TasksStorage.addTask(taskWithRepository);
@@ -75,6 +140,8 @@ public class CRUDTaskWithRepository {
         System.out.println("2. Ссылку на репозиторий");
         System.out.println("3. Текст задания");
         System.out.println("4. Пример");
+        System.out.println("5. Прикрепить задание к теме");
+        System.out.println("6. Открепить задание от темы");
         System.out.println("0. Выход");
         System.out.print("Выберите опцию: ");
 
@@ -116,6 +183,97 @@ public class CRUDTaskWithRepository {
                 System.out.println("Задание обновлено!");
                 break;
             }
+            case 5: {
+                if (CoursesStorage.getCourses().isEmpty()) {
+                    System.out.println("Список классов пуст, сперва создайте класс!");
+                    return;
+                }
+
+                CoursesStorage.writeAllCourses();
+                System.out.print("Выберите класс, к которому будет прикреплено задание: ");
+                int courseNumber = readIntInput();
+
+                if (courseNumber < 1 || courseNumber > CoursesStorage.getCourses().size()) {
+                    System.out.println("Ошибка, вы ввели неверный номер!");
+                    System.out.println("Задание не было прикреплено!");
+                    return;
+                }
+
+                Course course = CoursesStorage.getCourses().get(courseNumber - 1);
+                if (course.getModules().isEmpty()) {
+                    System.out.println("Список модулей пуст, сперва создайте модуль в классе!");
+                    return;
+                }
+
+                course.writeModules(course);
+                System.out.print("Выберите модуль, к которому будет прикреплено задание");
+                int moduleNumber = readIntInput();
+
+                if (moduleNumber < 1 || moduleNumber > course.getModules().size()) {
+                    System.out.println("Ошибка, вы ввели неверный номер!");
+                    System.out.println("Задание не было прикреплено!");
+                    return;
+                }
+
+                CourseModule module = course.getModules().get(moduleNumber - 1);
+                if (module.getTasks().contains(newTaskWithRepository)) {
+                    System.out.println("Модуль уже содержит данное задание, задание не было прикреплено повторно");
+                }
+                else {
+                    module.addTask(newTaskWithRepository);
+                    System.out.println("Задание прикреплено!");
+                }
+                break;
+            }
+            case 6: {
+                if (CoursesStorage.getCourses().isEmpty()) {
+                    System.out.println("Список классов пуст, задание никуда не прикреплено!");
+                    return;
+                }
+
+                CoursesStorage.writeAllCourses();
+                System.out.print("Выберите класс, от которого будет откреплено задание: ");
+                int courseNumber = readIntInput();
+
+                if (courseNumber < 1 || courseNumber > CoursesStorage.getCourses().size()) {
+                    System.out.println("Ошибка, вы ввели неверный номер!");
+                    System.out.println("Задание не было откреплено!");
+                    return;
+                }
+
+                Course course = CoursesStorage.getCourses().get(courseNumber - 1);
+                if (course.getModules().isEmpty()) {
+                    System.out.println("Список модулей пуст, задание не было откреплено!");
+                    return;
+                }
+
+                if (course.getCountModulesContainsTask(newTaskWithRepository) == 0) {
+                    System.out.println("Ни один модулем в этом класс не содержит данное задание!");
+                    return;
+                }
+
+                course.writeModulesContainsTask(newTaskWithRepository);
+                System.out.print("Выберите модуль, от которого будет откреплено задание: ");
+                int moduleNumber = readIntInput();
+
+                if (moduleNumber < 1 || moduleNumber > course.getCountModulesContainsTask(newTaskWithRepository)) {
+                    System.out.println("Ошибка, вы ввели неверный номер!");
+                    System.out.println("Задание не было откреплено!");
+                    return;
+                }
+
+                for (int i = 0; i < course.getModules().size(); ++i) {
+                    int counter = 0;
+                    if (course.getModules().get(i).getTasks().contains(newTaskWithRepository)) {
+                        counter++;
+                        if (counter == moduleNumber)  {
+                            course.getModules().get(i).removeTaskObject(newTaskWithRepository);
+                            System.out.println("Задание откреплено!");
+                        }
+                    }
+                }
+                break;
+            }
             case 0: {
                 return;
             }
@@ -139,6 +297,14 @@ public class CRUDTaskWithRepository {
         if (choice > TasksWithRepositoryStorage.getTasksWithRepository().size() || choice < 1) {
             System.out.println("Ошибка, вы ввели неверный номер!");
             return;
+        }
+
+        for (int i = 0; i < CoursesStorage.getCourses().size(); ++i) {
+            for (int j = 0; j < CoursesStorage.getCourses().get(i).getModules().size(); ++j) {
+                if (CoursesStorage.getCourses().get(i).getModules().get(j).getTasks().contains(TasksWithRepositoryStorage.getTasksWithRepository().get(choice - 1))) {
+                    CoursesStorage.getCourses().get(i).getModules().get(j).removeTaskObject(TasksWithRepositoryStorage.getTasksWithRepository().get(choice - 1));
+                }
+            }
         }
 
         TasksWithRepositoryStorage.removeTaskWithRepositoryAtIndex(choice - 1);
